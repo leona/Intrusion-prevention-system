965bd5b737692027f50b17e47b9b13b49484b965
Leon Harvey <leonharvey@live.com>
2015-06-23 22:47:36 +0000

    update

---
+++ modules/Starter Module/controller.php
@@ -0,0 +1,25 @@
+<?php
+namespace IPS\modules\Starter;
+
+class Controller extends BaseModule {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function __construct() {
+        
+    }
+    
+    public function startModule() {
+        
+    }
+    
+    public function endModule() {
+        
+    }
+    
+    public function execRoute($route) {
+        
+    }
+}
